588556c2d9077cb2ca79ccd0654f743b60843fa8
yskeum <rmadbstn@naver.com>
2022-07-04 23:01:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.lovetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navController : NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+     //   navController = nav_host_fragment.findNavController()
+        var navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        var navController = navHostFragment.navController
+
+    }
+}
